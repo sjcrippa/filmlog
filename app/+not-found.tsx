0d8e379a199634eed7a793511a1a0254c32fc53f
Santiago Julian Crippa <s.crippa@paisanos.io>
2024-10-13 02:42:20 -0300

    Initial layout

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,8 +1,8 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/natives/ThemedText';
+import { ThemedView } from '@/components/natives/ThemedView';
 
 export default function NotFoundScreen() {
   return (
